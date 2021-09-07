9101f67e963c0774b359c7e0f25c56829985fd82
fuchunhui <fuchuhu7@gmail.com>
2021-09-07 18:29:32 +0800

    feat: vue & plugin

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log('Hello World!');
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/meme-view/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/meme-view/' : '/',
+  plugins: [vue()]
 })
