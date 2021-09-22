1880d98f709dca4885bdfb8c80cd56ffd49167df
fuchunhui <fuchuhu7@gmail.com>
2021-09-22 18:32:38 +0800

    feat: copy plugin

--- scripts/copy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# rollup-plugin-copy 插件暂不兼容vite
-cp lib/index.html dist/
--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,10 +1,19 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/meme-view/' : '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        {src: 'lib/index.html', dest: 'dist'}
+      ],
+      hook: 'writeBundle'
+    })
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/index.ts'),
