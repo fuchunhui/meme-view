73ea6dcdfbab2c3db25c626b5f4b6720158e2d5f
fuchunhui <fuchuhu7@gmail.com>
2021-12-11 14:29:14 +0800

    release: v1.3.2

--- lib/index.ts
+++ lib/index.ts
@@ -2,11 +2,12 @@ import {createApp, ComponentPublicInstance} from 'vue';
 import App from '../src/App.vue';
 import '../src/assets/css/base.less';
 import '../src/assets/css/theme.less';
+import router from '../src/router';
 import {setDomain} from '../src/config';
 
 const load = (container: string | Element, domain: string): ComponentPublicInstance => {
   setDomain(domain || import.meta.env.VITE_APP_DOMAIN);
-  return createApp(App).mount(container);
+  return createApp(App).use(router).mount(container);
 };
 
 export default {
