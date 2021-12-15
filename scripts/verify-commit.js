27c6874542f0507553fcef2d025c1a65cd2352b3
fuchunhui <fuchuhu7@gmail.com>
2021-12-15 20:29:42 +0800

    fix: verify

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -5,15 +5,14 @@ import * as fs from 'fs';
 const __dirname = path.resolve();
 
 const msgPath = path.resolve(__dirname, '.git/COMMIT_EDITMSG');
-
 const msg = fs.readFileSync(msgPath, 'utf-8').trim();
 const rule = /^(revert: )?(feat|fix|docs|style|refactor|test|build|ci|chore|types|wip|release|perf)(\(.+\))?: .{1,50}/;
 
 if (!rule.test(msg)) {
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
-      chalk.red(`  Proper commit message format is required for automated changelog generation. \n  Examples:\n\n`) +
-      `${chalk.green(`    feat(compiler): add 'comments' option`)}\n`
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n`
+      + chalk.red(`  Proper commit message format is required for automated changelog generation. \n  Examples:\n\n`)
+      + `${chalk.green(`    feat(compiler): add 'comments' option`)}\n`
   );
   process.exit(1);
 }
